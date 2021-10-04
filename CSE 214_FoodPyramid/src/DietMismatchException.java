006038ffe677c711b0a7ef2e5df646344fcaef54
joohak123 <79282461+joohak123@users.noreply.github.com>
2021-10-04 18:14:27 -0400

    Delete CSE 214_FoodPyramid directory

--- CSE 214_FoodPyramid/src/DietMismatchException.java
+++
@@ -1,6 +0,0 @@
-
-public class DietMismatchException extends Exception {
-	public DietMismatchException() {
-		super("Error: This prey cannot be added as it does not match the idet of the predator.");
-	}
-}
--- CSE 214_FoodPyramid/src/PositionNotAvailableException.java
+++
@@ -1,6 +0,0 @@
-
-public class PositionNotAvailableException extends Exception {
-	public PositionNotAvailableException() {
-		super("Error: There is no more room for more prey for this predator.");
-	}
-}
